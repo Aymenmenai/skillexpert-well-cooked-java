e9deaa0cba0f34c77c3ad5031c7ed6c3290fc9a2
Aymenmenai <aymenmenai8@gmail.com>
2024-11-07 10:46:50 +0100

    add all the files

---
+++ task 011/answer.java
@@ -0,0 +1,16 @@
+// import java.util.Scanner;
+
+// class BinarySearch {
+//     public int search() {
+//         return 0;
+//     }
+
+//     public static void main(String args[]){
+//         Scanner in = new Scanner(System.in);
+//         int x = in.nextInt();
+//         in.close();
+//         int[] arr = {2,4,9,30 ,38 ,100};
+//         int length = arr.length;
+
+//     }
+// }
