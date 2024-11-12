0da17dfcc1ed3545eefe87660890704deef106c1
Aymenmenai <aymenmenai8@gmail.com>
2024-11-12 13:51:12 +0100

    15 challange done

---
+++ task 016/answer.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Aangram {
+    public static void main(String args[]) {
+        String word_1 = "Welcome";
+        String word_2 = "comewel";
+
+        char[] w1 = word_1.toLowerCase().toCharArray();
+        char[] w2 = word_2.toLowerCase().toCharArray();
+
+        // if (Arrays.compare(Arrays.sort(w1), Arrays.sort(w2))) {
+        //     System.out.println("Yes");
+        // } else {
+        //     System.out.println("Not");
+        // }
+
+    }
+}
